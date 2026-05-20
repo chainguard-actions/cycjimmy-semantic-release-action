fccd59f4a499a816e66e1564a072f678e7fa2bab
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2026-05-20 13:37:31 +0200

    Prune destination repo (keep only allowlisted files)

--- src/cleanupNpmrc.task.js
+++
@@ -1,9 +0,0 @@
-const io = require('@actions/io');
-
-/**
- * Clean up `.npmrc` file in the repo after releasing
- * @returns {Promise<never>}
- */
-module.exports = async () => {
-  await io.rmRF('.npmrc');
-};
